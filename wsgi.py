4cdf3bea532b7345666251c965c24684b69e1d2b
Juhani Heliö <juhani.helio@helsinki.fi>
2018-02-27 15:01:51 +0200

    Add logfile to persistent storage with time and host

--- wsgi.py
+++ wsgi.py
@@ -1,12 +1,15 @@
 import socket
+from datetime import datetime
 from flask import Flask
 
 application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    
-    return "Hello World! Greetings from "+socket.gethostname()+"\n"
+    data=socket.gethostname()+": "+str(datetime.now())
+    with open("/mnt/logfile", "w") as file:
+        file.write(data)
+    return data
 
 
 if __name__ == "__main__":
